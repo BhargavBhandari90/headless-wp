b19de46db62f6c7e81a001089ea33c95a1245557
Bunty <bn.bhandari90@gmail.com>
2023-04-13 00:07:05 +0530

    Add global styling using tailwind CsS

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -32,9 +32,25 @@ export default function Post({postData}) {
 
     return (
         <Layout>
-            <h1>{post.title.rendered}</h1>
-            {/* <div>{post.content.rendered}</div> */}
-            <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+            <section className="text-gray-400 bg-gray-900 body-font">
+              <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                <img
+                  className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+                  alt="hero"
+                  src="https://dummyimage.com/720x600"
+                />
+                <div className="text-center lg:w-2/3 w-full">
+                  <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                  {post.title.rendered}
+                  </h1>
+                  <p className="leading-relaxed mb-8">
+                    <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+                  </p>
+                </div>
+              </div>
+            </section>
+
+
         </Layout>
     );
 }
--- pages/blogs.js
+++ pages/blogs.js
@@ -17,15 +17,50 @@ export default function Blogs({allPostsData}) {
     return (
         <Layout>
             <h1>Blogs</h1>
-            <ul>
-            { allPostsData.map( ( post ) =>  (
-                <li key={post.id}>
-                    <Link href={'/blog/'+post.slug}>
-                        {post.title.rendered}
-                    </Link>
-                </li>
-            ) ) }
-            </ul>
+            <section className="text-gray-400 bg-gray-900 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap -m-4">
+                    { allPostsData.map( ( post ) =>  (
+                        <div className="p-4 md:w-1/3">
+                            <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
+                            <img
+                                className="lg:h-48 md:h-36 w-full object-cover object-center"
+                                src="https://dummyimage.com/720x400"
+                                alt="blog"
+                            />
+                            <div className="p-6">
+                                <h1 className="title-font text-lg font-medium text-white mb-3">
+                                {post.title.rendered}
+                                </h1>
+                                <p className="leading-relaxed mb-3">
+                                    <div dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
+                                </p>
+                                <div className="flex items-center flex-wrap ">
+                                <Link href={"blog/" + post.slug} className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                                    Learn More
+                                    <svg
+                                    className="w-4 h-4 ml-2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    >
+                                    <path d="M5 12h14" />
+                                    <path d="M12 5l7 7-7 7" />
+                                    </svg>
+                                </Link>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    ) ) }
+                    </div>
+                </div>
+                </section>
+
+
             <Link href="/">Go to Home!</Link>
         </Layout>
     );
