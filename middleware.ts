6c123fc1f0795e2e8a7ebec7db91a4c35ef79b4c
Bunty <bn.bhandari90@gmail.com>
2023-08-06 23:14:43 +0530

    Temp

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,36 @@
-import { withAuth } from "next-auth/middleware"
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {
-    console.log(req.nextauth.token)
+  function middleware(request: NextRequest) {
+    return NextResponse.rewrite(new URL( request.url ));
   },
   {
     callbacks: {
-      authorized: ({ token }) => { token?.role === "admin", }
-    },
+      authorized: async({ token }) => {
+
+        const req = await fetch(
+            process.env.NEXT_PUBLIC_JWT_BASE + 'token/validate',
+            {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization" : "Bearer " + token.accessToken,
+                }
+            }
+        );
+
+        const login_token = await req.json();
+
+        if ( login_token.code == 'jwt_auth_valid_token' ) {
+            return true;
+        }
+
+      }
+    }
   }
-);
+)
 
 export const config = { matcher: ["/", "/blogs"] }
