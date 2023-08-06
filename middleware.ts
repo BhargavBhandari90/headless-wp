63efb8d1d8ee9dea6937e235027efd0778428c88
Bunty <bn.bhandari90@gmail.com>
2023-08-07 00:34:21 +0530

    User prevention

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,34 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
-  function middleware(request: NextRequest) {
+  function middleware(request: NextResponse) {
     return NextResponse.rewrite(new URL( request.url ));
   },
   {
     callbacks: {
       authorized: async({ token }) => {
 
-        const req = await fetch(
-            process.env.NEXT_PUBLIC_JWT_BASE + 'token/validate',
-            {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization" : "Bearer " + token.accessToken,
-                }
-            }
-        );
+        if ( token?.accessToken ) {
 
-        const login_token = await req.json();
+          const req = await fetch(
+              process.env.NEXT_PUBLIC_JWT_BASE + 'token/validate',
+              {
+                  method: 'POST',
+                  headers: {
+                      "Content-Type": "application/json",
+                      "Authorization" : "Bearer " + token.accessToken,
+                  }
+              }
+          );
 
-        if ( login_token.code == 'jwt_auth_valid_token' ) {
-            return true;
+          const login_token = await req.json();
+
+          if ( login_token.code === 'jwt_auth_valid_token' ) {
+              return true;
+          }
+
         }
 
       }
